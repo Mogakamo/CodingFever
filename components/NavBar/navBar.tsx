5c36a037f7dc22693dba49bf58b11cb140985d0b
Mogakamo <mogaka.amo254@gmail.com>
2022-06-26 21:57:36 +0300

    fix: working on the responsiveness

--- components/NavBar/navBar.tsx
+++ components/NavBar/navBar.tsx
@@ -1,26 +1,38 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div className='h-16 shadow-md flex items-center px-28 justify-between'>
-        {/* Logo */}
-        <div>
-            <h1 className='text-6xl font-bold text-purple-900'>cF</h1>
-        </div>
+    <div className="h-16 w-auto shadow-md flex items-center md:px-28 justify-between px-4">
+      {/* Logo */}
+      <div>
+        <h1 className="md:text-6xl text-4xl font-bold text-purple-900">cF</h1>
+      </div>
+      <div className="hidden md:inline md:flex justify-between items-center space-x-20">
         {/* Quick links */}
         <div>
-            <ul className='flex space-x-10 text-lg font-semibold opacity-80'>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/">About</Link></li>
-                <li><Link href="/">Contact Us</Link></li>
-            </ul>
+          <ul className="flex space-x-10 text-lg font-semibold opacity-80">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+            <li>
+              <Link href="/">Contact Us</Link>
+            </li>
+          </ul>
         </div>
         <div>
-            <button className='rounded-full bg-purple-800 text-white px-6 py-2'>Get Started</button>
+          <button className="rounded-full bg-purple-800 text-white px-6 py-2">
+            Get Started
+          </button>
         </div>
+      </div>
+      {/* Menu Icon */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
